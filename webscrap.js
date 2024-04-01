60b626084716c44bfc0e6d4a0f6103a3949e625f
Leo Zhang <57935115+Leo10250@users.noreply.github.com>
2024-04-01 09:54:01 -0500

    Scrapes images from all Discover Resource Center article pages

--- webscrap.js
+++ webscrap.js
@@ -2,30 +2,52 @@ import fetch from 'node-fetch';
 import * as cheerio from 'cheerio';
 import fs from 'fs';
 
+const outputFile = 'articleImages.txt';
 async function scrapeSite(url) {
     const resposne = await fetch(url);
     const body = await resposne.text();
     const $ = cheerio.load(body);
-    const links = [];
-    $('a').each((index, element) => {
-        const link = $(element).attr('href');
-        if (
-            link &&
-            link.trim() !== '' &&
-            link.includes('https://www.discover.com')
-        ) {
-            links.push($.html($(element)));
-        }
-    });
-    return links;
+    let element = $('.blog-single-img');
+    if (element.length === 0) {
+        return '';
+    }
+    let image = element.attr('src');
+    return image;
 }
 
-const url =
-    'https://www.discover.com/personal-loans/resources/consolidate-debt/plan-after-recession/';
-scrapeSite(url)
-    .then((result) => {
-        fs.writeFile('Output.html', result.join('\n'), (err) => {
-            if (err) throw err;
-        });
-    })
-    .catch((err) => console.log(err));
+function parseString(file) {
+    let text = fs.readFileSync(file, 'utf8', function (err, data) {
+        if (err) throw err;
+    });
+    let list = text.split('\n');
+    return list;
+}
+
+function eraseFile(file) {
+    fs.writeFileSync(file, '', (err) => {
+        if (err) throw err;
+    });
+}
+
+function scrapeAllUrls() {
+    const site_list = parseString('page.txt');
+    eraseFile(outputFile);
+    let index = 0;
+    for (let url of site_list) {
+        scrapeSite(url)
+            .then((result) => {
+                fs.appendFileSync(outputFile, result, (err) => {
+                    if (err) throw err;
+                });
+                if (index !== site_list.length - 1) {
+                    fs.appendFileSync(outputFile, '\n', (err) => {
+                        if (err) throw err;
+                    });
+                }
+                index++;
+            })
+            .catch((err) => console.log(err));
+    }
+}
+
+scrapeAllUrls();
